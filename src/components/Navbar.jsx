931e8a5aa3608a5940e8d30a54509781f886b31e
Manny Toribio <manny.mtoribio@gmail.com>
2021-10-14 15:04:01 -0400

    react app for classic cars

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import './navbar.css'
+
+function Navbar () {
+  return (
+    <header>
+      <nav>
+        <section>Classic Cars ForLife</section>
+        <section>Shop 70's</section>
+        <section>Shop 80's</section>
+        <section>Account Login</section>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
